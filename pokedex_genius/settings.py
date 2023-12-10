195a0570468a9856af55c9a2611f8e3f42178834
NatashaRy <natasha@briljantkommuikation.se>
2023-12-10 12:19:31 +0100

    feat: updated statics to cloudinary

--- pokedex_genius/settings.py
+++ pokedex_genius/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = (
+    'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+    )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
