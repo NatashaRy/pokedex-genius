5b6976bb38ecc51b489d2aa2389e19b01706ad4d
NatashaRy <natasha@briljantkommuikation.se>
2023-12-11 11:51:19 +0100

    feat: added dashboard + pokedex model

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Pokedex(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    # featured_image = models.ImageField(upload_to='media/')
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
