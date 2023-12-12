190b806dab5ffb2b17e6cd95599c50dd925b6ff5
NatashaRy <natasha@briljantkommuikation.se>
2023-12-12 18:06:43 +0100

    feat: added profile picture to pokedex user

--- users/models.py
+++ users/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class pokedexUser(AbstractUser):
     date_of_birth = models.DateField(blank=True, null=True)
     website_url = models.URLField(blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True, null=True
+    )
