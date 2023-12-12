37da3d34300972e9d8ed1d30e2a9c44d0d805fb2
NatashaRy <natasha@briljantkommuikation.se>
2023-12-12 16:55:45 +0100

    fix: updated styles

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    """
-    Render the index.html template
-    """
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     return render(request, 'core/index.html')
