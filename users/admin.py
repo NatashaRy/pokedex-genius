714c39d74fe9110fbbe78a51b3e58dda37fa2aca
NatashaRy <natasha@briljantkommuikation.se>
2023-12-13 01:03:00 +0100

    feat: added admin management

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import pokedexUser
 
-# Register your models here.
+
+class pokedexUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_joined', 'date_of_birth')
+    list_filter = ('date_joined',)
+    search_fields = ('username', 'email')
+    ordering = ('-date_joined',)
+
+
+admin.site.register(pokedexUser, pokedexUserAdmin)
