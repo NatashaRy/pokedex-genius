ee9291841550e4c0f35102759b0a5a4c84ac9ddb
NatashaRy <natasha@briljantkommuikation.se>
2023-12-15 11:10:07 +0100

    feat: added add to pokemon on pokemon_details.html

--- pokedex/forms.py
+++ pokedex/forms.py
@@ -1,7 +1,25 @@
 from django import forms
+from datetime import datetime
 
 
 class PokemonDropdown(forms.Form):
     def __init__(self, choices=None, *args, **kwargs):
         super(PokemonDropdown, self).__init__(*args, **kwargs)
         self.fields['pokemon'] = forms.ChoiceField(choices=choices or [])
+
+
+class AddPokemonForm(forms.Form):
+    pokedex_id = forms.IntegerField()
+    date_added = forms.DateTimeField(
+        initial=datetime.now(),
+        widget=forms.HiddenInput()
+        )
+    additional_notes = forms.CharField(
+        max_length=200,
+        widget=forms.Textarea, required=False
+        )
+
+    class Meta:
+        fields = ('pokedex_id',
+                  'date_added'
+                  'additional_notes')
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Pokedex
+from .models import Pokedex, UserPokemon
 import requests
-from .forms import PokemonDropdown
+from .forms import PokemonDropdown, AddPokemonForm
 
 
 def dashboard(request):
@@ -50,9 +50,22 @@ def pokemon_details(request, entry_number):
         print(e)
         pokemon_details = {}
 
+    if request.method == 'POST':
+        add_pokemon_form = AddPokemonForm(request.POST)
+        if add_pokemon_form.is_valid():
+            UserPokemon.objects.create(
+                user=request.user,
+                pokemon_id=entry_number,
+                pokedex_id=request.objects.get('pokedex')
+            )
+            # Toast comfirmation message
+    else:
+        add_pokemon_form = AddPokemonForm()
+
     return render(request,
                   'pokedex/pokemon_details.html',
-                  {'pokemon': pokemon_details}
+                  {'pokemon': pokemon_details,
+                   'form': add_pokemon_form}
                   )
 
 
@@ -61,3 +74,22 @@ class PokedexList(generic.ListView):
     queryset = Pokedex.objects.filter(status=1).order_by('-created_on')
     template_name = 'pokedex/dashboard.html'
     paginate_by = 9
+
+
+
+
+
+
+
+
+# Characteristics
+# Abilities: Each Pokémon may have one or more abilities that can affect battles and gameplay. Abilities can have various effects, such as boosting stats, preventing status conditions, or influencing weather.
+# Stats: Pokémon have six core stats: HP (Hit Points), Attack, Defense, Special Attack, Special Defense, and Speed. These stats determine how well a Pokémon performs in battles and can be influenced by their species, level, and individual values (IVs).
+# Evolution: Many Pokémon can evolve into a different species when they reach a certain level, meet specific conditions, or use certain items. Evolution can significantly impact a Pokémon's stats and appearance.
+# Moves and Attacks: Pokémon can learn a variety of moves and attacks. The moves they know can be crucial in battles, and they can learn new moves as they level up or through other means.
+# Egg Groups: Pokémon are categorized into different egg groups, which determine their breeding compatibility. Pokémon in the same egg group can breed to produce offspring.
+# Pokedex Number: Each Pokémon has a unique Pokedex number that helps identify it within the game's Pokedex.
+# Shiny Variation: Some Pokémon have a rare "shiny" variation with different coloration. Shiny Pokémon are highly sought after by collectors.
+# Gigantamax/Dynamax Forms: In certain Pokémon games, some species can undergo Gigantamax or Dynamax transformations, granting them increased size and power in specific battles.
+# Habitat: Pokémon can be found in various in-game habitats, such as forests, caves, or water bodies.
+# Legendary and Mythical Pokémon: These are special, often unique, Pokémon with significant roles in the game's storyline.
