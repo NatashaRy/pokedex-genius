108d11fb845f62101bc23b421925d2bff4016a56
NatashaRy <natasha@briljantkommuikation.se>
2023-12-21 22:10:06 +0100

    fix: remove public choice from admin

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -3,14 +3,14 @@ from .models import Pokedex, UserPokemon
 
 
 class PokedexAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id', 'user', 'created_on', 'is_public')
-    list_filter = ('is_public', 'created_on')
+    list_display = ('name', 'id', 'user', 'created_on')
+    list_filter = ('created_on',)
     search_fields = ('name', 'user__username')
 
 
 class UserPokemonAdmin(admin.ModelAdmin):
-    list_display = ('user', 'pokedex', 'pokemon_id', 'date_added', 'is_favorite')
-    list_filter = ('pokedex', 'user')
+    list_display = ('user', 'pokedex', 'pokemon_id', 'date_added', 'is_favorite',)
+    list_filter = ('pokedex', 'user',)
     search_fields = ('pokedex__name', 'user__username')
 
 
