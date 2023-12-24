6f337ff15e655551bd75cd5252def9e80138e666
NatashaRy <natasha@briljantkommuikation.se>
2023-12-24 18:18:11 +0100

    fix: updated allowed hosts

--- pokedex_genius/settings.py
+++ pokedex_genius/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['pokedex-genius-9617911b5f35.herokuapp.com/',
+ALLOWED_HOSTS = ['pokedex-genius-9617911b5f35.herokuapp.com',
                  'localhost', '127.0.0.1']
 
 # Authentication settings
