838a77542bac48b62dcb3de25b8a7210ab327a76
NatashaRy <natasha@briljantkommuikation.se>
2023-12-25 15:14:25 +0100

    fix: collectedstatic files

--- pokedex_genius/settings.py
+++ pokedex_genius/settings.py
@@ -185,10 +185,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
